9434b294c4890e0d70602e46293f5c905f376d36
SUHYUNSHIM <lisa9963suhyun@gmail.com>
2021-07-18 22:17:31 +0900

    Posts, jpa

---
+++ src/main/java/org/example/webTest/domain/posts/Posts.java
@@ -0,0 +1,32 @@
+package org.example.webTest.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity //엔티티 클래스.
+public class Posts {
+    @Id //해당 테이블의 primary key field
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length =500 , nullable =false) //테이블의 칼럼
+    private String title;
+
+    @Column(columnDefinition ="TEXT",nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author){
+        this.title= title;
+        this.content = content;
+        this.author = author;
+    }
+
+}
