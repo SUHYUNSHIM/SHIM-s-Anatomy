915b1a56d69803eb04855f96477045507322b857
suhyunshim <lisa9963suhyun@gmail.com>
2021-09-19 14:53:03 +0900

    네이버 로그인

--- src/main/java/org/example/webTest/web/IndexController.java
+++ src/main/java/org/example/webTest/web/IndexController.java
@@ -1,6 +1,7 @@
 package org.example.webTest.web;
 
 import lombok.RequiredArgsConstructor;
+import org.example.webTest.config.auth.LoginUser;
 import org.example.webTest.config.auth.dto.SessionUser;
 import org.example.webTest.service.posts.PostsService;
 import org.example.webTest.web.dto.PostsResponseDto;
@@ -34,10 +35,18 @@ public class IndexController {
         return "posts-update";
     }
 
-    @GetMapping("/")
+ /*   @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
         SessionUser user = (SessionUser)httpSession.getAttribute("user");
+        if(user !=null){
+            model.addAttribute("userName",user.getName());
+        }
+        return "index";
+    }*/
+    @GetMapping("/")
+    public String index(Model model, @LoginUser SessionUser user){
+        model.addAttribute("posts",postsService.findAllDesc());
         if(user !=null){
             model.addAttribute("userName",user.getName());
         }
